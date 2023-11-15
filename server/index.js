6f501f5727c4a4a691e2a2b0c297a6c75ad0a78e
reymaa19 <rcmaala@gmail.com>
2023-11-15 16:47:12 -0600

    create api requests for getting players and their stats

--- server/index.js
+++ server/index.js
@@ -5,16 +5,48 @@ const API_URI = process.env.API_URI
 
 const express = require('express')
 const cors = require('cors')
+
 require('express-async-errors')
+
 const app = express()
 
 app.use(cors())
 
-app.get('/api/players', async (req, res) => {
-  console.log(API_URI)
-  const response = await fetch(API_URI)
+app.get('/api/players/:search', async (req, res) => {
+  const response = await fetch(
+    `${API_URI}/players?search=${req.params.search}&per_page=100`
+  )
   const result = await response.json()
-  res.send(result)
+  const players = result.data.map((p) => {
+    const details = {
+      id: p.id,
+      name: `${p.first_name} ${p.last_name}`,
+      position: p.position,
+    }
+
+    return details
+  })
+
+  res.send(players)
+})
+
+app.get('/api/stats/:id', async (req, res) => {
+  const response = await fetch(
+    `${API_URI}/stats?seasons[]=2023&player_ids[]=${req.params.id}&per_page=100`
+  )
+  const result = await response.json()
+  const stats = { gms: 0, pts: 0, ast: 0, reb: 0, blk: 0, stl: 0 }
+  const allStats = result.data.reduce((prev, curr) => {
+    prev.pts += curr.pts
+    prev.ast += curr.ast
+    prev.reb += curr.reb
+    prev.blk += curr.blk
+    prev.stl += curr.stl
+    prev.gms += curr.min !== '00' && 1
+    return prev
+  }, stats)
+
+  res.send(allStats)
 })
 
 app.listen(PORT, () => {

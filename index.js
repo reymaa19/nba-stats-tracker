e75c9666da751265f3c53587c922d7789e3f581e
reymaa19 <47261404+reymaa19@users.noreply.github.com>
2024-10-15 15:30:21 -0500

    Deploy vercel

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
-require('dotenv').config()
-const playersRouter = require('./routers/players')
-const statsRouter = require('./routers/stats')
-const middleware = require('./utils/middleware')
-const express = require('express')
-const mongoose = require('mongoose')
-const path = require('path')
+require('dotenv').config();
+const playersRouter = require('./routers/players');
+const statsRouter = require('./routers/stats');
+const middleware = require('./utils/middleware');
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
 
+// Connect to MongoDB
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => console.log('connected to MongoDB'))
-  .catch((error) => console.log('error connecting to MongoDB:', error.message))
+  .catch((error) => console.log('error connecting to MongoDB:', error.message));
 
-const app = express()
+const app = express();
 
+// Middleware and routes
 app
   .use(express.static(path.join(__dirname, 'dist')))
   .use(express.json({ limit: '50mb' }))
   .use(middleware.requestLogger)
   .use('/api/players', playersRouter)
   .use('/api/stats', statsRouter)
-  .use(middleware.errorHandler)
-  .listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`)
-  })
+  .use(middleware.errorHandler);
+
+// Export the app for serverless deployment
+module.exports = app;
